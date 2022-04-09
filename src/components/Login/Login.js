99cb6d88711cd3e50c5ed0bb385331be6411e7d2
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-09 22:50:15 +0600

    sing up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./Login.css";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+    const nagative=useNavigate();
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  if(user){
+    nagative('/shop')
+  }
+
+  const handleusesingin = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email,password);
+  };
+
   return (
     <div className="from-container">
       <div>
         <h1 className="from-title">Login</h1>
-        <form action="">
+        <form onSubmit={handleusesingin}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" />
+            <input
+              onBlur={handleEmail}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="Password" id="" />
+            <input
+              onBlur={handlePassword}
+              type="password"
+              name="Password"
+              id=""
+              required
+            />
           </div>
-          <input className="from-submit" type="submit" value="Login" />
+          <p style={{color:'red'}}>{error?.message}</p>
+          {
+              loading && <p>Loadding....</p>
+          }
+          <input className="from-submit" type="submit" value="Login" required />
         </form>
-        <p>New To Ema-John?<Link className="from-link" to='/singup'>Create an account</Link></p>
+        <p>
+          New To Ema-John?
+          <Link className="from-link" to="/singup">
+            Create an account
+          </Link>
+        </p>
       </div>
     </div>
   );
